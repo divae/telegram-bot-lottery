4c9c60d5422be1e7fc9c37d81d86a7d8fa56b8cf
Estela Medrano Jiménez <stlmedrano@gmail.com>
2021-01-06 10:09:38 +0100

    Change to children lottery

--- chatbot/constants.py
+++ chatbot/constants.py
@@ -1,5 +1,5 @@
 import os
 
 TELEGRAM_WEBHOOK_URL = f'https://api.telegram.org/bot{os.environ["TELEGRAM_WEBHOOK_KEY"]}/'  # <-- add your telegram token as environment variable
-URL_CHRISTMAS_LOTTERY = 'https://api.elpais.com/ws/LoteriaNavidadPremiados'
+URL_CHRISTMAS_LOTTERY = 'https://api.elpais.com/ws/LoteriaNinoPremiados'
 URL_CHRISTMAS_LOTTERY_SUMMARY = f'{URL_CHRISTMAS_LOTTERY}?n=resumen'
